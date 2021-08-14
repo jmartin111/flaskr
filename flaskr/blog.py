ccc74b80415449422ced3e0c5bbab8b21a3c2da6
Jeff Martin <chess.logic@gmail.com>
2021-08-13 23:20:11 -0700

    Bug finding mission
    * More practice is definitely in order
    * You can do this, Jeff

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,96 +1,1 @@
-from flask import (
-    Blueprint, g, request, redirect, url_for, flash, render_template
-)
-
-from werkzeug.exceptions import abort
-
-from flaskr.auth import login_required
-from flaskr.db import get_db
-
-bp = Blueprint('blog', __name__)
-
-
-@bp.route('/')
-def index():
-    """This view is the 'homepage' and will display
-       all posts that have been submitted
-    """
-    db = get_db()
-    posts = db.execute(
-        "SELECT p.id, title, body, created, author_id, username"
-        "   FROM post p JOIN user u on p.author_id = u.id"
-        "   ORDER BY created DESC"
-    ).fetchall()
-
-    return render_template('blog/index.html', posts=posts)
-
-
-@bp.route('/create', methods=('GET', 'POST'))
-@login_required
-def create_post():
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        error = None
-
-        if not title:
-            error = "A title must be supplied"
-
-        if error:
-             flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                "INSERT INTO post (title, body, author_id)"
-                "   VALUES (?, ?, ?)",
-                (title, body, g.user['id'])
-            )
-            db.commit()
-            return redirect(url_for('blog.index'))
-
-    return render_template('blog/index.html')
-
-
-def get_post(post_id, check_author=True):
-    post = get_db().execute(
-        "SELECT p.id, title, body, created, author_id, username"
-        "   FROM post p JOIN user u on p.author_id = u.id"
-        "   WHERE p.id = ?",
-        (post_id,)
-    ).fetchone()
-
-    if post is None:
-        abort(404, f"Post id [{post_id}] doesnt exist.")
-
-    if check_author and post['author_id'] != g.user['id']:
-        abort(403)
-
-    return post
-
-
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
-@login_required
-def update_post():
-    post = get_post(id)
-
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        error = None
-
-    if not title:
-        error = "Title is required"
-
-    if error is not None:
-        flash(error)
-    else:
-        db = get_db()
-        db.execute(
-            "UPDATE post SET title = ?, body = ?"
-            "   WHERE id = ?",
-            (title, body, id)
-        )
-        db.commit()
-        return redirect(url_for('blog.index'))
-
-    render_template('blog/update.html', post=post)
+from flask import (    Blueprint, g, request, redirect, url_for, flash, render_template)from werkzeug.exceptions import abortfrom flaskr.auth import login_requiredfrom flaskr.db import get_dbbp = Blueprint('blog', __name__)@bp.route('/')def index():    """This view is the 'homepage' and will display       all posts that have been submitted    """    db = get_db()    posts = db.execute(        "SELECT p.id, title, body, created, author_id, username"        "   FROM post p JOIN user u on p.author_id = u.id"        "   ORDER BY created DESC"    ).fetchall()    return render_template('blog/index.html', posts=posts)@bp.route('/create', methods=('GET', 'POST'))@login_requireddef create_post():    if request.method == 'POST':        title = request.form['title']        body = request.form['body']        error = None        if not title:            error = "A title must be supplied"        if error:             flash(error)        else:            db = get_db()            db.execute(                "INSERT INTO post (title, body, author_id)"                "   VALUES (?, ?, ?)",                (title, body, g.user['id'])            )            db.commit()            return redirect(url_for('blog.index'))    return render_template('blog/create.html')def get_post(post_id, check_author=True):    post = get_db().execute(        "SELECT p.id, title, body, created, author_id, username"        "   FROM post p JOIN user u on p.author_id = u.id"        "   WHERE p.id = ?",        (post_id,)    ).fetchone()    if post is None:        abort(404, f"Post id [{post_id}] doesnt exist.")    if check_author and post['author_id'] != g.user['id']:        abort(403)    return post@bp.route('/<int:post_id>/update', methods=('GET', 'POST'))@login_requireddef update_post(post_id):    post = get_post(post_id)    title, error, body = None, None, None    if request.method == 'POST':        title = request.form['title']        body = request.form['body']        error = None    if not title:        error = "Title is required"    if error is not None:        flash(error)    else:        db = get_db()        db.execute(            "UPDATE post SET title = ?, body = ?"            "   WHERE id = ?",            (title, body, post_id)        )        db.commit()        return redirect(url_for('blog.index'))    return render_template('blog/update.html', post=post)@bp.route('/<int:id>/delete')@login_requireddef delete_post():    db = get_db()    db.execute("DELETE from post WHERE id = ?", (id,))    db.commit()    return redirect(url_for('blog.index'))
